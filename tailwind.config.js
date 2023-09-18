4a241d4009617f070c0d17b2fe983f431e602d4a
Oleksii Vilchanskyi <o.wilczanski@gmail.com>
2023-09-18 14:03:26 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Montserrat', 'sans-serif'],
+        'serif': ['Fraunces', 'serif']
+      },
+      colors: {
+        'dark-cyan': 'hsl(158, 36%, 37%)',
+        'cream': 'hsl(30, 38%, 92%)',
+        'very-dark-blue': 'hsl(212, 21%, 14%)',
+        'dark-grayish-blue': 'hsl(228, 12%, 48%)'
+      }
+    },
+  },
+  plugins: [],
+}
